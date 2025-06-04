72a2417b1805e81dae95406dd17315378b3e28b6
sj.kim <sj.kim@greendote.co.kr>
2025-06-04 17:01:39 +0900

    feat. main.js 최적화

--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
-function showTopBar(){
-    let country = "France";
-    let vat = 20;
-    setTimeout(() => {
-        document.querySelector("section.country-bar")
-        .innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`
-        document.querySelector("section.country-bar")
-        .classList.remove('hidden')
-    }, 1000);
+function showTopBar() {
+  const country = "France";
+  const vat = 20;
+
+  const countryBar = document.querySelector("section.country-bar");
+  if (countryBar) {
+    countryBar.innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`;
+    countryBar.classList.remove("hidden");
+  }
 }
 
-
-
 showTopBar();
-
-
-
-
